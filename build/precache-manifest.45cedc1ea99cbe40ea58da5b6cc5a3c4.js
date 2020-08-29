213ae5a5fe246ff0aec7399d9504c811104b5e04
Abhijeet Singh <43774040+Abhijeet199@users.noreply.github.com>
2020-08-29 18:36:09 +0530

    adding production folder

---
+++ build/precache-manifest.45cedc1ea99cbe40ea58da5b6cc5a3c4.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800140e35b9884bfeabacc70b216aba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0bbd728e825d8044118",
+    "url": "/static/css/2.7d8cb836.chunk.css"
+  },
+  {
+    "revision": "9cd3f41950f04f2cd57f",
+    "url": "/static/css/main.f082ff2b.chunk.css"
+  },
+  {
+    "revision": "c0bbd728e825d8044118",
+    "url": "/static/js/2.8d61225c.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.8d61225c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd3f41950f04f2cd57f",
+    "url": "/static/js/main.99e60e1b.chunk.js"
+  },
+  {
+    "revision": "2ec73dfa7c292d7b1b32",
+    "url": "/static/js/runtime-main.9bde171e.js"
+  },
+  {
+    "revision": "9404b473c8c18cf590d1951cbe2d4f41",
+    "url": "/static/media/1.9404b473.webp"
+  },
+  {
+    "revision": "a37820db9214d20f75e7e247747ef037",
+    "url": "/static/media/2.a37820db.webp"
+  },
+  {
+    "revision": "8a4add4c399330f6fb3939cd95b07907",
+    "url": "/static/media/3.8a4add4c.webp"
+  },
+  {
+    "revision": "39544e7c3de7092fd3e8fa7eff46c71d",
+    "url": "/static/media/4.39544e7c.webp"
+  },
+  {
+    "revision": "213dc5c7a5a9c18550a3d79c76c3f0b3",
+    "url": "/static/media/5.213dc5c7.webp"
+  },
+  {
+    "revision": "9412d5646234a435c2ff364c0065aa7c",
+    "url": "/static/media/6.9412d564.webp"
+  },
+  {
+    "revision": "fd4d48a9415e1db0249956712193e67f",
+    "url": "/static/media/aloo-kulcha.fd4d48a9.jpg"
+  },
+  {
+    "revision": "ab4fc4ab5fd84f6ae0074441bdd83748",
+    "url": "/static/media/butter-naan.ab4fc4ab.jpg"
+  },
+  {
+    "revision": "6fbcf3c3e97e98d7f8f40c84e3dd7a8a",
+    "url": "/static/media/butter-roti.6fbcf3c3.jpg"
+  },
+  {
+    "revision": "2f4dfe3fb1ec45cc802fea1e37c51640",
+    "url": "/static/media/chole.2f4dfe3f.jpg"
+  },
+  {
+    "revision": "2caa3cedece1c765ead017894b83163f",
+    "url": "/static/media/corn_soup.2caa3ced.jpg"
+  },
+  {
+    "revision": "942b10893209d12134d9192bf5f781fc",
+    "url": "/static/media/dahi-ke-kabab.942b1089.jpg"
+  },
+  {
+    "revision": "c6b0752a50f344699268e12b5803d32a",
+    "url": "/static/media/dal-makhani.c6b0752a.jpg"
+  },
+  {
+    "revision": "9deafc56f09a0689baa0332ba266d6e9",
+    "url": "/static/media/empty_cart.9deafc56.svg"
+  },
+  {
+    "revision": "71b9fe2b637d7b3b527a4aa09a634bd4",
+    "url": "/static/media/fresh-veg.71b9fe2b.png"
+  },
+  {
+    "revision": "cf468d7435020c8d70a8f9b048ed91a5",
+    "url": "/static/media/garlic-naan.cf468d74.jpg"
+  },
+  {
+    "revision": "abbc55294de2ec02f680210bc0178532",
+    "url": "/static/media/gulab-jamun.abbc5529.jpg"
+  },
+  {
+    "revision": "bfb6d0610ed6171940a14e1eb56e165c",
+    "url": "/static/media/halwa.bfb6d061.jpg"
+  },
+  {
+    "revision": "3acc1554a8a66664fc60f24066066820",
+    "url": "/static/media/hara-bhara-kabab.3acc1554.jpg"
+  },
+  {
+    "revision": "fa3e0b3d91559a40c78127f1ebdbc526",
+    "url": "/static/media/hyderabadi-biryani.fa3e0b3d.jpg"
+  },
+  {
+    "revision": "341436573a3bd99cb791650b35b38c4a",
+    "url": "/static/media/laccha-paratha.34143657.jpg"
+  },
+  {
+    "revision": "82e211fb7262fd3eb9b9f868269d07ad",
+    "url": "/static/media/malai-kofta.82e211fb.jpg"
+  },
+  {
+    "revision": "03b5c08aacf7d0a1e6bd142b1b125600",
+    "url": "/static/media/malai-paneer-tikka.03b5c08a.jpg"
+  },
+  {
+    "revision": "93894454adecb4360d1ef6241d0c43f0",
+    "url": "/static/media/moong-dal.93894454.jpg"
+  },
+  {
+    "revision": "3a412b82b0fbc120f79329f30135e2d4",
+    "url": "/static/media/onion-kulcha.3a412b82.jpg"
+  },
+  {
+    "revision": "28c0eede26f7cad4230a9fe2d5df8640",
+    "url": "/static/media/paneer-butter-masala.28c0eede.jpg"
+  },
+  {
+    "revision": "35e86ea8e0cfd4da063dcca619b2f5d9",
+    "url": "/static/media/paneer-kulcha.35e86ea8.jpg"
+  },
+  {
+    "revision": "c1298ecc0bf8e751ed8da08e2bf7ef88",
+    "url": "/static/media/papad.c1298ecc.jpg"
+  },
+  {
+    "revision": "be22d0c10af8e2c3c821bed7bf7a5ca3",
+    "url": "/static/media/paratha.be22d0c1.jpg"
+  },
+  {
+    "revision": "1b735d67e585666befa639f0373b5ff9",
+    "url": "/static/media/plain-naan.1b735d67.jpg"
+  },
+  {
+    "revision": "927c31560b7f8129361ad8a126d54490",
+    "url": "/static/media/plate.927c3156.png"
+  },
+  {
+    "revision": "46c63fbbcc06fe0d9c1905abe89f694f",
+    "url": "/static/media/plate2.46c63fbb.png"
+  },
+  {
+    "revision": "8cc7038139ffda73214459734131acb1",
+    "url": "/static/media/pudina-paratha.8cc70381.jpg"
+  },
+  {
+    "revision": "c3916f5d06df215ffa00e6422b1cac99",
+    "url": "/static/media/pulao.c3916f5d.jpg"
+  },
+  {
+    "revision": "56ebe7b75d05a91e7ed7196a2a4f7650",
+    "url": "/static/media/raita.56ebe7b7.jpg"
+  },
+  {
+    "revision": "43807249a9d5e615c44d057a68069bdc",
+    "url": "/static/media/rajma.43807249.jpg"
+  },
+  {
+    "revision": "2925a21feb2b88bfddcde68b67fff409",
+    "url": "/static/media/regular-thali.2925a21f.jpg"
+  },
+  {
+    "revision": "686438604cc13d6e5dc6082b03b045b1",
+    "url": "/static/media/roomali-roti.68643860.jpg"
+  },
+  {
+    "revision": "8306d198e73d8d2bcaf201f15167250c",
+    "url": "/static/media/roti.8306d198.jpg"
+  },
+  {
+    "revision": "07ab85d53c67b1c39b063614ef723f1b",
+    "url": "/static/media/shahi-paneer.07ab85d5.jpg"
+  },
+  {
+    "revision": "47f14ef7508367ded35801489d0bba52",
+    "url": "/static/media/soya-chaap.47f14ef7.jpg"
+  },
+  {
+    "revision": "0039a55067ebcc502101d7648cac102a",
+    "url": "/static/media/special_thali.0039a550.jpg"
+  },
+  {
+    "revision": "98c94c2e4157581def17545114bd24ac",
+    "url": "/static/media/steamed-rice.98c94c2e.jpg"
+  },
+  {
+    "revision": "7f2f631d5027f2ed3a8e1f438d5b9529",
+    "url": "/static/media/table.7f2f631d.jpg"
+  },
+  {
+    "revision": "8c55d0aef5a2d8ccb31226c8bf4c96d9",
+    "url": "/static/media/tandoori-aloo.8c55d0ae.jpg"
+  },
+  {
+    "revision": "720a7f76260b7c7db0839410be01414f",
+    "url": "/static/media/tandoori_paneer_tikka.720a7f76.jpg"
+  },
+  {
+    "revision": "dc81a4a07a27468c345552de9f5684e9",
+    "url": "/static/media/tomato_soup.dc81a4a0.jpg"
+  }
+]);
